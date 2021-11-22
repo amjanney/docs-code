45fd8a632c5c8cf66c3238bfc07098bbd45a428d
xiumubai <1547702880@qq.com>
2021-11-22 18:56:32 +0800

    feat：router

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -22,13 +22,14 @@ import './index.css';
 // import App from './advanced-guides/refs';
 // import App from './advanced-guides/render-props/MouseTracker';
 // import App from './advanced-guides/render-props/MouseTracker3';
-import App from './hooks';
-
+// import App from './hooks';
+import App from './router'
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ react/my-app/src/router/READE.md
@@ -0,0 +1,2 @@
+[v4 docs](https://v5.reactrouter.com/web/example/nesting)
+[v6 docs](https://reactrouter.com/docs/en/v6)
