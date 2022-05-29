dbfd18e119c0be0134f20d89df0336048b64fa3c
xiumubai <1547702880@qq.com>
2022-05-30 00:44:08 +0800

    feat: 二分查找

--- algorithm/查找/binarySearch.js
+++ algorithm/查找/binarySearch.js
@@ -2,24 +2,86 @@
  * @Author: xiumubai 1547702880@qq.com
  * @Date: 2022-05-28 16:51:36
  * @LastEditors: xiumubai 1547702880@qq.com
- * @LastEditTime: 2022-05-28 22:21:49
+ * @LastEditTime: 2022-05-30 00:40:10
  * @FilePath: /docs-code/javascript/数据结构与算法/查找/binarySearch.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 二分查找
  */
 
-// 1-100000 找到数字50000
+const DOES_NOT_EXIST = 'dose not exist!';
+const Compare = {
+  /** 小于 */
+  LESS_THAN: -1,
+  /** 大于 */
+  BIGGER_THAN: 1,
+  /** 等于 */
+  EQUALS: 0,
+};
 
-var arr = [1,2,3,4,5,6,7,8,9]
-
-
-function binnarySearch(array, value) {
-  // const sortedArray = quikSort(array)
-  let min  = 0;
-  let max =  array.length - 1;
-  let mid = 0;
-  while(min < max) {
-    if (array[mid] < min) {
-      min ++
-    } else if()
+/**
+ * 比较两个值的大小
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} -1 1 0
+ */
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
   }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
 }
+
+/**
+ * @description: 插入排序
+ * @param {*} array
+ * @param {*} compareFn
+ * @return {*}
+ */
+function insertionSort(array, compareFn = defaultCompare) {
+  const { length } = array;
+  let temp;
+  for (let i = 1; i < length; i++) {
+    let j = i;
+    temp = array[i];
+    while (j > 0 && compareFn(array[j - 1], temp) === Compare.BIGGER_THAN) {
+      array[j] = array[j - 1];
+      j--;
+    }
+    array[j] = temp;
+  }
+  return array;
+}
+
+/**
+ * @description: 二分查找
+ * @param {*} array
+ * @param {*} value
+ * @param {*} compareFn
+ * @return {*}
+ */
+function binnarySearch(array, value, compareFn = defaultCompare) {
+  const sortedArray = insertionSort(array);
+  let low = 0;
+  let high = sortedArray.length - 1;
+  while (lesserOrEquals(low, high, compareFn)) {
+    const mid = Math.floor(low + high);
+    const ele = sortedArray[mid];
+    if (compareFn(ele, value) === Compare.LESS_THAN) {
+      low = mid + 1;
+    } else if (compareFn(ele, value) === Compare.BIGGER_THAN) {
+      high = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+  return DOES_NOT_EXIST;
+}
+
+function lesserOrEquals(a, b, compareFn) {
+  const comp = compareFn(a, b);
+  return comp === Compare.LESS_THAN || cpmp === Compare.EQUALS;
+}
+
+const arr = [5, 4, 5, 3, 6, 4, 2, 1];
+console.log('未排序的数组', arr);
+const num = binnarySearch(arr, 6);
+console.log('查找到的数据', num);
